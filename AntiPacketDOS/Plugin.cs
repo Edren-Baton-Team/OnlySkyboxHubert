9d5e3b53615239f55993816763cc606fb3909675
Rysik5318 <72207886+Rysik5318@users.noreply.github.com>
2023-11-12 17:18:41 +0400

    Add files via upload

---
+++ AntiPacketDOS/Plugin.cs
@@ -0,0 +1,27 @@
+﻿using Exiled.API.Features;
+using HarmonyLib;
+using System;
+
+namespace AntiPacketDOS
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Prefix => "OnlySkyboxHubert";
+        public override string Name => "OnlySkyboxHubert";
+        public override string Author => "Rysik5318";
+        public override Version Version => new Version(1, 0, 0);
+        internal Harmony harmony;
+
+        public override void OnEnabled()
+        {
+            harmony = new Harmony($"OnlySkyboxHubert - {DateTime.Now}");
+            harmony.PatchAll();
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            harmony.UnpatchAll();
+            base.OnDisabled();
+        }
+    }
+}
